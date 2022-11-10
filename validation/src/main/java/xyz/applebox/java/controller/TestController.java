8d538caaa6a95fdb59f6407782827e83ecf700ae
jini <jini@jiniworld.me>
2022-11-10 17:03:30 +0900

    :sparkles: parameter validation check 추가

--- validation/src/main/java/xyz/applebox/java/controller/TestController.java
+++ validation/src/main/java/xyz/applebox/java/controller/TestController.java
@@ -1,22 +1,28 @@
 package xyz.applebox.java.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import xyz.applebox.java.model.BaseResponse;
 import xyz.applebox.java.model.Member;
 import xyz.applebox.java.model.User;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.util.Map;
 
+@Validated
 @RequestMapping("/test")
 @RestController
 public class TestController {
 
-    @GetMapping("/size")
-    public Map sizeCheck(@Size(min = 2, max = 5) String name) {
-        return Map.of(name, name.length());
+    @GetMapping("/param")
+    public Map paramCheck(
+            @Size(min = 2, max = 5) @RequestParam String name,
+            @Min(1) @Max(130) @RequestParam Integer age) {
+        return Map.of(name, name.length(), "age", age);
     }
 
     @PostMapping("/user")
