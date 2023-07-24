3abcdbafc9c848484ca094981ffa684ee43bd1b8
jini <jini@jiniworld.me>
2023-07-25 00:16:00 +0900

    :heavy_plus_sign: spring-batch 모듈 추가

---
+++ spring-batch/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+}
