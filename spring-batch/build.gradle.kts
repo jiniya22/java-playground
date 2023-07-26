dc0cd23f00affb2754a698577a8f28d745a4e99f
jini <jini@jiniworld.me>
2023-07-27 07:24:38 +0900

    :sparkles: mysql profile 설정을 이용한 기본 배치 테스트

--- spring-batch/build.gradle.kts
+++ spring-batch/build.gradle.kts
@@ -3,4 +3,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java")
 }
