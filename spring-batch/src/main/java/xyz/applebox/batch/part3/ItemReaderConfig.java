5262713a4c2b55cf587f410ce034543a84f60445
jini <jini@jiniworld.me>
2023-08-09 04:26:53 +0900

    :sparkles: JdbcCursorItemReader를 이용한 예제

--- spring-batch/src/main/java/xyz/applebox/batch/part3/ItemReaderConfig.java
+++ spring-batch/src/main/java/xyz/applebox/batch/part3/ItemReaderConfig.java
@@ -1,6 +1,7 @@
 package xyz.applebox.batch.part3;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -8,9 +9,12 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,12 +25,14 @@ import java.util.stream.Collectors;
 public class ItemReaderConfig {
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
+  private final DataSource dataSource;
 
   @Bean
   public Job itemReaderJob() {
     return jobBuilderFactory.get("itemReaderJob")
             .incrementer(new RunIdIncrementer())
             .start(customReaderStep())
+            .next(jdbcStep())
             .build();
   }
 
@@ -37,6 +43,28 @@ public class ItemReaderConfig {
             .reader(new CustomItemReader<>(getItems()))
             .writer(itemWriter())
             .build();
+  }
+
+  @Bean
+  public Step jdbcStep() {
+    return stepBuilderFactory.get("jdbcStep")
+            .<Person, Person>chunk(10)
+            .reader(jdbcCursorItemReader())
+            .writer(itemWriter())
+            .build();
+  }
+
+  @SneakyThrows
+  private JdbcCursorItemReader<Person> jdbcCursorItemReader() {
+    JdbcCursorItemReader<Person> itemReader = new JdbcCursorItemReaderBuilder<Person>()
+            .name("jdbcCursorItemReader")
+            .dataSource(dataSource)
+            .sql("select id, name, age, address from person")
+            .rowMapper((rs, rowNum) ->
+                    new Person(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4)))
+            .build();
+    itemReader.afterPropertiesSet();
+    return itemReader;
   }
 
   private ItemWriter<Person> itemWriter() {
