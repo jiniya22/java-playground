348c78a8101816bf475e6ea3067ed7248b0e0559
jini <jini@jiniworld.me>
2024-03-10 15:20:52 +0900

    :sparkles: effective-java 모듈 추가

---
+++ effective-java/src/main/java/me/jiniworld/effectivejava/EffectiveJavaApplication.java
@@ -0,0 +1,14 @@
+package me.jiniworld.effectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        System.out.println(">>>>");
+        SpringApplication.run(EffectiveJavaApplication.class, args);
+    }
+
+}
